7be0b393e2f9380bca3fab1031c53fa907e2a4bc
viettrungIT3 <viettrungcntt03@gmail.com>
2022-06-12 00:51:10 +0700

    create theme HomeTransaction

--- QuanLyNhaDat/src/quanlynhadat/views/users/HomeTransactionFrame.java
+++ QuanLyNhaDat/src/quanlynhadat/views/users/HomeTransactionFrame.java
@@ -8,12 +8,12 @@ package quanlynhadat.views.users;
  *
  * @author Trung
  */
-public class LandTransactionFrame extends javax.swing.JFrame {
+public class HomeTransactionFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form LandTransactionFrame
      */
-    public LandTransactionFrame() {
+    public HomeTransactionFrame() {
         initComponents();
     }
 
@@ -27,7 +27,7 @@ public class LandTransactionFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("QUẢN LÝ ĐẤT");
+        setTitle("QUẢN LÝ NHÀ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -61,20 +61,21 @@ public class LandTransactionFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LandTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LandTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LandTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LandTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomeTransactionFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LandTransactionFrame().setVisible(true);
+                new HomeTransactionFrame().setVisible(true);
             }
         });
     }
