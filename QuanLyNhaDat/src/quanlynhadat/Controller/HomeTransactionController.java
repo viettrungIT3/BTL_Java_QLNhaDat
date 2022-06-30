914270ee4a47d48a818521f08bb45f2e664560c0
viettrungIT3 <viettrungcntt03@gmail.com>
2022-06-30 17:45:03 +0700

    add feature - 'HomeTransactionsRevenueFrame'

--- QuanLyNhaDat/src/quanlynhadat/Controller/HomeTransactionController.java
+++ QuanLyNhaDat/src/quanlynhadat/Controller/HomeTransactionController.java
@@ -160,4 +160,56 @@ public class HomeTransactionController {
             return false;
         }
     }
+    
+     // Lấy toàn bộ danh sách giao dịch theo năm
+    public static List<Transaction> getAllTransactionByYear(String year) {
+        List<Transaction> list = new ArrayList();
+
+        try {
+            Connection conn = ConnectDB.getConnection();
+            String sqlHome = "SELECT * FROM HOME_TRANSACTIONS where  YEAR(t_date)='" + year + "' ORDER BY FORMAT(t_date, 'yyyy/MM/dd') DESC, t_id, t_type, t_price";
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(sqlHome);
+            while (rs.next()) {
+                Date day = rs.getDate("t_date");
+                Date myday = null;
+                if (day != null) {
+                    myday = new Date(day.getTime());
+                    Transaction trs = new Transaction(rs.getInt("t_id"), myday, rs.getFloat("t_price"), rs.getString("t_type"), rs.getFloat("t_area"), rs.getInt("id"), 1);
+                    list.add(trs);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    // Lấy toàn bộ danh sách giao dịch theo tháng năm
+    public static List<Transaction> getAllTransactionByMonthYear(String year, String month) {
+        List<Transaction> list = new ArrayList();
+
+        try {
+            Connection conn = ConnectDB.getConnection();
+            String sqlHome = "SELECT * FROM HOME_TRANSACTIONS where YEAR(t_date)='" + year + "' and Month(t_date)='" + month + "'ORDER BY FORMAT(t_date, 'yyyy/MM/dd') DESC, t_id, t_type, t_price";
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(sqlHome);
+            while (rs.next()) {
+                Date day = rs.getDate("t_date");
+                Date myday = null;
+                if (day != null) {
+                    myday = new Date(day.getTime());
+                    Transaction trs = new Transaction(rs.getInt("t_id"), myday, rs.getFloat("t_price"), rs.getString("t_type"), rs.getFloat("t_area"), rs.getInt("id"), 1);
+                    list.add(trs);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+
 }
